d78b20f2361806d7cb2d952c0e1bc58b2aa8c177
DanielYorke <62666085+DanielYorke@users.noreply.github.com>
2023-11-30 05:36:58 +0000

    modified for implemetation

--- App/models/host.py
+++ App/models/host.py
@@ -2,14 +2,18 @@ from App.database import db
 
 class Host(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    organizationName =  db.Column(db.String, nullable=False, unique=True)
-    #website = db.Column(db.String, nullable=True)
-    
+    organizationName =  db.Col
     #competitions = db.relationship("CompetitionHost", lazy=True, backref=db.backref("competitions"), cascade="all, delete-orphan")
+
+    def __init__(self,id ,name):
+        self.id = id
+        self.name = name
 
     def toDict(self):
         res = {
-            "id": self.id,
+            "id": self.id,umn(db.String, nullable=False, unique=True)
+    #website = db.Column(db.String, nullable=True)
+    
             "name": self.organizationName,
             #"website": self.website
         }
